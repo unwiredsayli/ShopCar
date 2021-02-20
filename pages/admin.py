39c0758174da702b36c4c52cc9934f8e54601249
sayli <saylishimpi77@gmail.com>
2021-02-20 16:22:25 +0530

    added cars app and fetched the data into homepage

--- pages/admin.py
+++ pages/admin.py
@@ -7,7 +7,7 @@ class TeamAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.photo.url))
 
-    thumbnail.short_discription = 'Photo'
+    thumbnail.short_description = 'Photo'
 
     list_display = ('id','thumbnail','first_name','designation','created_date')
     list_display_links = ('id','thumbnail','first_name',)
